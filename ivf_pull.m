098cc279359da546d9c6bbf13b934267adc1e70a
jagdish <jagdish@mozark.ai>
2022-01-07 11:23:47 +0530

    device connectivity added with runner pull

--- ivf_pull.m
+++ ivf_pull.m
@@ -291,8 +291,54 @@ void list_devs( ucmd *cmd ) {
             const char *name = [[device localizedName] UTF8String];
             const char *id = [[device uniqueID] UTF8String];
             //index            = [devices count] + [devices_muxed indexOfObject:device];
-            printf("--Device--\n  Name:%s\n  UDID:%s\n", name, id );
+            printf("--List Device--\n  Name:%s\n  UDID:%s\n", name, id );
         }
+
+        NSNotificationCenter *nCenter = [NSNotificationCenter defaultCenter];
+        // Setup an observer for newly connected devices
+        id observeConnect = [
+            nCenter addObserverForName:AVCaptureDeviceWasConnectedNotification
+            object:nil
+            queue:[NSOperationQueue mainQueue]
+            usingBlock:^(NSNotification *note)
+        {
+            NSArray<AVMediaType> *device_types = [NSArray arrayWithObjects: AVCaptureDeviceTypeExternalUnknown, nil];
+            AVCaptureDeviceDiscoverySession *session = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:device_types mediaType:AVMediaTypeMuxed position:0];
+            NSArray<AVCaptureDevice *> *devices = [session devices];
+        
+            uint8_t count = [devices count];
+            for (AVCaptureDevice *device in devices) {
+                const char *name = [[device localizedName] UTF8String];
+                const char *id = [[device uniqueID] UTF8String];
+                //index            = [devices count] + [devices_muxed indexOfObject:device];
+                printf("-- AVCaptureDeviceWasConnectedNotification --Device--\n  Name:%s\n  UDID:%s\n", name, id );
+            }
+        }];
+
+        // Setup an observer for disconnected devices
+        id observeDisconnect = [
+             nCenter addObserverForName:AVCaptureDeviceWasDisconnectedNotification
+             object:nil
+             queue:[NSOperationQueue mainQueue]
+             usingBlock:^(NSNotification *note)
+        {
+            NSArray<AVMediaType> *device_types = [NSArray arrayWithObjects: AVCaptureDeviceTypeExternalUnknown, nil];
+            AVCaptureDeviceDiscoverySession *session = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:device_types mediaType:AVMediaTypeMuxed position:0];
+            NSArray<AVCaptureDevice *> *devices = [session devices];
+        
+            uint8_t count = [devices count];
+            for (AVCaptureDevice *device in devices) {
+                const char *name = [[device localizedName] UTF8String];
+                const char *id = [[device uniqueID] UTF8String];
+                //index            = [devices count] + [devices_muxed indexOfObject:device];
+                printf("--Disconnect Device--\n  Name:%s\n  UDID:%s\n", name, id );
+            }
+        }];
+        
+        [[NSRunLoop currentRunLoop] run];
+
+        [nCenter removeObserver:observeConnect];
+        [nCenter removeObserver:observeDisconnect];
     }
 }
 
@@ -386,6 +432,7 @@ int run_stream( ucmd *cmd, char *udidIn, int nanoOut, char *outFile, int verbose
         AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:foundDevice error:&error];
         if( input == nil ) {
             NSLog(@"%@", error);
+            printf(@"Error input device %@", error);
             return false;
         }
         
